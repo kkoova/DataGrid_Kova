a688ed6d7cd3c8245919facf52bbcda3c23d6a2f
1101-22 <alya.korobeynikova99@gmail.com>
2024-11-16 12:12:46 +0300

    ///

---
+++ MemoryStorage.Entiti/DataGridContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Contracts.Models;
+
+namespace MemoryStorage.Entiti
+{
+    public class DataGridContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
