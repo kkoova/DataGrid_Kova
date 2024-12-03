a900937a6fbcca34842bdcbb01a4baba624e899e
Korobeynikova <Korobeynikova@B329-L01>
2024-12-03 10:42:45 +0300

    errrorr

--- DataGridCar.Web.Page/Program.cs
+++ DataGridCar.Web.Page/Program.cs
@@ -1,27 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
+using DataGridCar.CarManagerData;
+using DataGridCar.Contracts;
+using DataGridCar.DataStorage.Entity;
+using Serilog;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace DataGridCar.Web.Page
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Seq("http://localhost:5341/", apiKey: "9yhGAscQDNIfQNmlZJuf")
+            .CreateLogger();
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddSingleton<ICarStorage, DataBaseCarStorage>();
+            builder.Services.AddScoped<ICarManager, CarManager>();
+            builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
